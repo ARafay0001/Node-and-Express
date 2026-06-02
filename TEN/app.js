39e702418a646906326c1b648b270f6d054e1ce1
arafay0001 <abdulrafayimran4@gmail.com>
2026-06-02 23:41:06 +0500

    express routes

--- TEN/app.js
+++ TEN/app.js
@@ -1,39 +1,26 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 
 const app = express()
 
-
-app.use((req, res, next) => {
- console.log('first dummy middleware', req.url, req.method)
- next()
+app.get('/', (req, res, next) => {
+  res.send('<a href="/add-home"> ADD Home </a>');
+})
+app.use(express.urlencoded())
+app.get('/add-home', (req, res, next) => {
+  res.send(`
+<h1> Register your home here: </h1>
+<form action="/add-home" method="POST">
+<input type="text" name="houseName"
+placeholder="Enter the name of your house" />
+<input type="submit" />
+</form>`)
+})
+app.post('/add-home', (req, res, next) => {
+  console.log(req.body);
+  res.send(`
+<h1> Register Sucessfully </h1>
+`)
 })
 
-app.use((req, res, next) => {
- console.log('Secind dummy middleware', req.url, req.method)
- next()
-})
-
-app.get('/',(req, res, next) => {
- res.send(`<p>response from third middleware go to <a href="http://localhost:3000/contact">contact</a></p> `)
-})
-
-app.get('/contact',(req, res, next) => {
- res.send(`<p>please give your details</p>
-  <form action="/contact" method="post">
-    <input type="text" name="name" placeholder="name"><br>
-    <input type="email" name="email" id="" placeholder="email"><br>
-    <input type="submit" >
-  </form>
-  `)
-})
-
-app.use(bodyParser.urlencoded())
-
-app.post('/contact', (req, res, next) => {
-  console.log(req.body)
-  res.send(`<p>response sent we will contact you shortly</p>`)
-})
-
-
-app.listen(3000)
+app.listen(3000);
