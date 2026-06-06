decf65a9fdc676c8cfe0987f6f45425498b6d9a4
arafay0001 <abdulrafayimran4@gmail.com>
2026-06-06 19:28:02 +0500

    MVC controllers

--- 13/app.js
+++ 13/app.js
@@ -23,7 +23,8 @@ app.use('/host',hostRouter)
 app.use((req, res, next) => {
   // res.status(404).res.sendFile(path.join(__dirname, './views/404.html'))
   
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
-})
+  // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+  res.status(404).render('404') })
+
 
 app.listen(3000);
---
+++ 13/controllers/home.js
@@ -0,0 +1,3 @@
+exports.addhome = (req, res, next) => {
+  res.render('form')}
+  
